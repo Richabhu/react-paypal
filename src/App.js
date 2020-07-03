80e8ffc9f36be042b3ad5a7b645ea03af7fe052a
Richabu <richa.bhuwania@hashedin.com>
2020-07-03 17:40:19 +0530

    react integration with Paypal gateway

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import PaypalButtons from './components/PaypalButtons'
+
+class App extends Component {
+  state = {
+    showPaypal: false
+  };
+
+  showPaypalButtons = () => {
+    this.setState({ showPaypal: true });
+  };
+
+  render() {
+    const { showPaypal } = this.state;
+    if (showPaypal) {
+      return <PaypalButtons />;
+    } else {
+      return (
+        <div className="main">
+          <h2> Buy this Mercedes at a giveaway price (Super Cheap) </h2>
+          <img alt="Mercedes G-Wagon" src="https://www.cstatic-images.com/car-pictures/xl/USC10TSC011A0101.png" />
+          <h3>
+            <b>$200</b>
+          </h3>
+          <button onClick={this.showPaypalButtons}> Pay </button>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
